6e51ad17fe07ea45a2a175c9cf23f7df29a2c1b4
yash <yashmevat16@gmail.com>
2024-07-14 10:27:14 +0530

    success

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,8 +1,6 @@
-import React, { useContext } from 'react'
-import noteContext from '../context/notes/noteContext'
+import React from 'react'
 
 const About = () => {
-const a = useContext(noteContext)
   return (
     <div>
       this is about page
